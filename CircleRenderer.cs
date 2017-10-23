1a61d0226ec15c884c757306bb8611a5b954e2e6
V0LKoF <33032655+V0LKoF@users.noreply.github.com>
2017-10-24 01:08:15 +0900

    Add files via upload

---
+++ CircleRenderer.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+
+public class CircleRenderer : LineDrawer {
+    
+   // public LineRenderer linerender;
+    public int LinesCount;
+    private float xi, yi,x=0,y=0;
+    // Use this for initialization
+    void Start () {
+        
+  
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    /*   public void DrawCircle() {
+           float x = 0, y=0;
+           LinesCount = linerender.positionCount;
+           linerender.positionCount = LinesCount + 1;
+                xi = 0;yi = 0;
+           Vector3 v = new Vector3(xi, yi, 0);
+           linerender.SetPosition(LinesCount, v);
+            for(int i=0;i<LinesCount+1;i++)
+              {
+               xi = x + 200 * Mathf.Cos(Mathf.PI * (2 * i + 1) / LinesCount );
+               yi = y + 200 * Mathf.Sin(Mathf.PI * (2 * i + 1) / LinesCount );
+               Vector3 v1 = new Vector3(xi, yi, 0);
+               linerender.SetPosition(i,v1);
+               }*/
+
+    public void DrawCircle()
+    {
+        xi = 0;yi = 0;
+          Clear();
+        LinesCount++;
+        linerender.positionCount = LinesCount+1;
+        for (int i = 0; i < LinesCount + 1; i++)
+        {
+            xi = x + 200 * Mathf.Cos(Mathf.PI * (2 * i + 1) / LinesCount);
+            yi = y + 200 * Mathf.Sin(Mathf.PI * (2 * i + 1) / LinesCount);
+            DrawLine(xi, yi, 0, i);
+        }
+
+    }
+
+    public void drawstar()
+    {
+        float a = 0;
+        xi = 0; yi = 0;
+        Clear();
+        LinesCount++;
+        linerender.positionCount = LinesCount + 1;
+              for (int i = 0; i < LinesCount-1 ; i++)
+        {
+            if (i % 2 == 0)
+            {
+                xi = x + 200 / 2 * Mathf.Cos(a );
+                yi = y - 200 / 2 * Mathf.Sin(a );
+            }
+            else
+            {
+                xi = x + 150 * Mathf.Cos(a );
+                yi = y - 150 * Mathf.Sin(a );
+            }
+            a = a + Mathf.PI / LinesCount;
+            DrawLine(xi, yi, 0, i);
+        }
+       DrawLine(100, 0, 0, LinesCount);
+    }
+
+
+}
+
---
+++ LineDrawer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineDrawer : MonoBehaviour {
+    public LineRenderer linerender;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("escape"))
+            Application.Quit();
+    }
+    public void DrawLine(float x, float y, float z/*, float x1, float y1, float z1,*/,int pos)
+      {
+       // linerender.positionCount = linerender.positionCount + 2;
+        Vector3 v = new Vector3(x,y,z);
+     //   Vector3 v4 = new Vector3(x1, y1, z1);
+        linerender.SetPosition(pos, v);
+      //  linerender.SetPosition( pos +1, position: v4);
+
+    }
+    public void Clear()
+         {
+        for (int i = 0; i < linerender.positionCount ; i++) { 
+            Vector3 v = new Vector3(0, 0, 0);
+        linerender.SetPosition(i, v);
+        }
+    }
+
+ 
+
+    public void Close()
+    {
+        Application.Quit();
+    }
+  
+    }
